22dba06719b46f204bdeef6fd90c82ac2120a104
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2022-12-26 12:50:36 +0200

    for now

--- src/Project/Core/Utils.java
+++ src/Project/Core/Utils.java
@@ -112,6 +112,31 @@ public class Utils {
 
         gl.glDisable(GL.GL_BLEND);
     }
+    public static void DrawSprite2(GL gl, int x, int y, int index, int[] textures, int w, int h) {
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]); // Turn Blending On
+
+        gl.glPushMatrix();
+        gl.glTranslated(-5, -5, 0);
+        gl.glBegin(GL.GL_QUADS);
+        // Front Face
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(x, y + h, -1.0f);
+
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(x + w, y + h, -1.0f);
+
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(x + w, y, -1.0f);
+
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(x, y, -1.0f);
+
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
+    }
 
     public static void drawString(GL gl, int x, int y, String s) {
         GLUT glt = new GLUT();
