508c7eafd50402f3df2cbf6b11ac9b65596b0c7a
Farah <farahhossam121@gmail.com>
2022-12-26 19:23:58 +0200

    edit for Map4

--- src/Project/Maps/Map4/Map4Listener.java
+++ src/Project/Maps/Map4/Map4Listener.java
@@ -1,57 +1,91 @@
 package Project.Maps.Map4;
 
 import Project.Core.AnimListener;
+import Project.Core.texture.TextureReader;
 import Project.Models.Directions;
 import Project.Models.Eating;
+import Project.Models.Ghost;
 import Project.Models.Pacman;
+import Project.Pages.GameOver;
 
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLException;
+import javax.media.opengl.glu.GLU;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.BitSet;
 
 import static Project.Core.Utils.*;
 import static java.awt.event.KeyEvent.*;
-import static java.awt.event.KeyEvent.VK_LEFT;
 import static java.lang.Math.*;
-import static java.lang.Math.toRadians;
 
 public class Map4Listener extends AnimListener {
 
     Pacman pacman = new Pacman();
-    ArrayList<Eating> eating = new ArrayList<>();
 
-    static final int MAX_X = 260;
-    static final int MAX_Y = 200;
+    JFrame frame = null;
+    Clip eatingSound;
+
+    int cntFood;
+    int cntLives = 3;
+    ArrayList<Eating> eating = new ArrayList<>();
+    static final int MAX_X = 240;
+    static final int MAX_Y = 250;
+
+    ArrayList<Ghost> ghosts = new ArrayList<>();
+    static int GHOSTS_SIZE = 4;
+
+    int animIndexForPacman = 1;
+    int animIndexForFood = 1;
+
+    String[] textureNames = {
+            "map4.jpg", // 0
+            "pacman.png", // 1
+            "up.gif", // 2
+            "down.gif", // 3
+            "right.gif", // 4
+            "left.gif", // 5
+            "ghost.gif", // 6
+            "food.png", // 7
+            "food2.png", // 8
+    };
+    TextureReader.Texture[] texture = new TextureReader.Texture[textureNames.length];
+    int[] textures = new int[textureNames.length];
 
     int[][] map = new int[][]{
-            {1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-            {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+            {0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0},
+            {0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+            {0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0},
+            {0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0},
+            {0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+            {0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0},
+            {0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+            {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     };
     int row = map.length;
     int col = map[0].length;
-
 
     public void init(GLAutoDrawable gld) {
 
@@ -59,19 +93,55 @@ public class Map4Listener extends AnimListener {
 
         gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+        {
+            gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
+            gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+            gl.glGenTextures(textureNames.length, textures, 0);
+
+            for (int i = 0; i < textureNames.length; i++) {
+                try {
+                    texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i], true);
+                    gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
+
+                    new GLU().gluBuild2DMipmaps(
+                            GL.GL_TEXTURE_2D,
+                            GL.GL_RGBA, // Internal Texel Format,
+                            texture[i].getWidth(), texture[i].getHeight(),
+                            GL.GL_RGBA, // External format from image,
+                            GL.GL_UNSIGNED_BYTE,
+                            texture[i].getPixels() // Image data
+                    );
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+
         gl.glMatrixMode(GL.GL_PROJECTION);
         gl.glLoadIdentity();
 
         gl.glOrtho(0, MAX_X, MAX_Y, 0, 0, 1.0);
 
         fillEating();
+
+        addGhostsToArray();
+        for (Ghost g : ghosts) {
+            g.randMove();
+        }
+    }
+
+    private void addGhostsToArray() {
+        for (int i = 0; i < GHOSTS_SIZE; i++) {
+            ghosts.add(new Ghost(200, 200));
+        }
     }
 
     private void fillEating() {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (map[i][j] == 1) { // eat
-                    eating.add(new Eating(j, i));
+                    eating.add(new Eating(i, j));
                 }
             }
         }
@@ -83,19 +153,81 @@ public class Map4Listener extends AnimListener {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
 
 
+        drawBackground(gl);
+
+
         gl.glPushMatrix();
-//        gl.glScaled(0.5,0.5,1);
+        gl.glTranslated(-2.5, 5, 0);
+        gl.glScaled(1.07, 0.99, 1);
         {
-            drawBackground(gl);
+
             drawEating(gl);
             drawPacman(gl);
-
-            handelPacmanMove();
-            handelPacmanEating();
-
-            handelWinning();
+            drawGhost(gl);
         }
         gl.glPopMatrix();
+
+
+        handelPacmanMove();
+        handelPacmanEating();
+
+        handelGhostMove();
+
+        handelLose();
+
+        handelWinning();
+
+        try {
+            drawString(gl, 5, MAX_Y - 8, "Score: " + cntFood);  // Score
+            drawString(gl, 60, MAX_Y - 8, "Lives: " + cntLives); // Lives
+        } catch (GLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    private void handelGhostMove() {
+        for (Ghost g : ghosts) {
+            switch (g.direction) {
+                case IDEAL -> {
+                }
+                case UP -> {
+                    if (g.y - g.step < 0 || g.jj - 1 < 0 || map[g.jj - 1][g.ii] == 0) {
+                        g.randMove();
+                        return;
+                    }
+                    g.moveUP();
+                }
+                case DOWN -> {
+                    if (g.y + g.step > MAX_Y || g.jj + 1 >= row || map[g.jj + 1][g.ii] == 0) {
+                        g.randMove();
+                        return;
+                    }
+                    g.moveDown();
+                }
+                case RIGHT -> {
+                    if (g.x + g.step > MAX_X || g.ii + 1 >= col || map[g.jj][g.ii + 1] == 0) {
+                        g.randMove();
+                        return;
+                    }
+                    g.moveRight();
+                }
+                case LEFT -> {
+                    if (g.x - g.step < 0 || g.ii - 1 < 0 || map[g.jj][g.ii - 1] == 0) {
+                        g.randMove();
+                        return;
+                    }
+                    g.moveLeft();
+                }
+            }
+        }
+    }
+
+    private void drawGhost(GL gl) {
+        for (Ghost g : ghosts) {
+            DrawSprite(gl, (int) g.x, (int) g.y, 6, textures, Ghost.R);
+        }
     }
 
     private void handelWinning() {
@@ -105,16 +237,33 @@ public class Map4Listener extends AnimListener {
         }
     }
 
-    private void handelPacmanEating() {
+    private void handelLose() {
+        for (Ghost g : ghosts) {
+            if (g.ii == pacman.ii && g.jj == pacman.jj) {
+                if (eatingSound != null) eatingSound.stop();
+                if (--cntLives == 0) {
+                    frame.dispose();
+                    new GameOver().setVisible(true);
+                } else {
+                    pacman.reset();
+                }
+            }
 
+        }
+    }
+
+    private void handelPacmanEating() {
         for (int i = 0; i < eating.size(); i++) {
             if (pacman.ii == eating.get(i).ii && pacman.jj == eating.get(i).jj) {
-//                System.out.println(i);
-                System.out.println(eating.size());
+                cntFood++;
+                if (eatingSound == null || !eatingSound.isRunning()) {
+                    eatingSound = playMusic("src/Project/Assets/pacman-wakawaka.wav", false);
+                }
                 eating.remove(i--);
             }
         }
     }
+
 
     private void handelPacmanMove() {
         if (isKeyPressed(VK_UP)) {
@@ -137,21 +286,19 @@ public class Map4Listener extends AnimListener {
             case IDEAL -> {
             }
             case UP -> {
-                if (pacman.y - pacman.step < 0 || pacman.jj - 1 < 0 || map[pacman.jj - 1][pacman.ii] == 0) return;
+                if (pacman.y - pacman.step < 0 || map[pacman.ii][pacman.jj - 1] == 0) return;
                 pacman.moveUP();
             }
             case DOWN -> {
-                if (pacman.y + pacman.step > MAX_Y || pacman.jj + 1 >= row || map[pacman.jj + 1][pacman.ii] == 0)
-                    return;
+                if (pacman.y + pacman.step > 100 || map[pacman.ii][pacman.jj + 1] == 0) return;
                 pacman.moveDown();
             }
             case RIGHT -> {
-                if (pacman.x + pacman.step > MAX_X || pacman.ii + 1 >= col || map[pacman.jj][pacman.ii + 1] == 0)
-                    return;
+                if (pacman.x + pacman.step > 100 || map[pacman.ii + 1][pacman.jj] == 0) return;
                 pacman.moveRight();
             }
             case LEFT -> {
-                if (pacman.x - pacman.step < 0 || pacman.ii - 1 < 0 || map[pacman.jj][pacman.ii - 1] == 0) return;
+                if (pacman.x - pacman.step < 0 || map[pacman.ii - 1][pacman.jj] == 0) return;
                 pacman.moveLeft();
             }
         }
@@ -159,12 +306,42 @@ public class Map4Listener extends AnimListener {
 
     private void drawEating(GL gl) {
         for (Eating e : eating) {
-            drawCircle(gl, 1, new Color(255, 255, 255), e.x, e.y);
+            DrawSprite(gl, (int) e.x, (int) e.y, 7, textures, 4);
         }
     }
 
     private void drawPacman(GL gl) {
-        drawCircle(gl, Pacman.R, new Color(255, 255, 1), pacman.x, pacman.y);
+        // check Dir for motion
+        changeAnimIndex();
+
+        DrawSprite(gl, (int) pacman.x, (int) pacman.y, animIndexForPacman, textures, Pacman.R);
+    }
+
+    private void changeAnimIndex() {
+        switch (pacman.direction) {
+            case IDEAL -> {
+                animIndexForPacman = 1;
+            }
+            case UP -> { // 2
+                if (animIndexForPacman == 1) animIndexForPacman = 2;
+                else if (animIndexForPacman == 2) animIndexForPacman = 1;
+            }
+            case DOWN -> { // 3
+
+                if (animIndexForPacman == 1) animIndexForPacman = 3;
+                else if (animIndexForPacman == 3) animIndexForPacman = 1;
+            }
+            case RIGHT -> { // 4
+
+                if (animIndexForPacman == 1) animIndexForPacman = 4;
+                else if (animIndexForPacman == 4) animIndexForPacman = 1;
+            }
+            case LEFT -> { // 5
+
+                if (animIndexForPacman == 1) animIndexForPacman = 5;
+                else if (animIndexForPacman == 5) animIndexForPacman = 1;
+            }
+        }
     }
 
     private void drawCircle(GL gl, int r, Color color, double x, double y) {
@@ -184,16 +361,8 @@ public class Map4Listener extends AnimListener {
 
 
     private void drawBackground(GL gl) {
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (map[i][j] == 0) { // blocks
-                    double x = arcTrX(j) - 5, y = arcTrY(i) - 5;
+        DrawSprite(gl, 5, 5, 0, textures, MAX_X);
 
-                    drawRect(gl, x, y, 10, 10, 0, 0, 1);
-
-                }
-            }
-        }
     }
 
 
